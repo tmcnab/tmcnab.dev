bcc762547cd65cdea5e38599fd356153822336d1
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2024-09-22 23:31:46 -0400

    update flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Page () {
 	return (
-		<h1>Hello world</h1>
+		<>
+			<h1>Hello world</h1>
+			<br />
+			<p>My name is Tristan: a programmer, engineer, husband and socialist. This website attempts to catalogue my previous and current activities.</p>
+		</>
 	)
 }
--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -2,15 +2,16 @@ import { CSSProperties, ReactNode } from "react"
 
 export interface FlexProps {
 	children: ReactNode
+	justifyContent: 'space-around' | 'space-between' | 'center' | 'end' | 'space-evenly' | 'start'
 	vertical?: boolean
 }
 
-export default function Flex ({ children, vertical = false }: FlexProps) {
+export default function Flex ({ children, justifyContent = 'space-between', vertical = false }: FlexProps) {
 	const style: CSSProperties = {
 		display: 'flex',
 		flexDirection: vertical ? 'column' : 'row',
+		justifyContent,
 		gap: '1rem',
-		justifyContent: 'space-between',
 	}
 
 	return <div children={children} style={style} />

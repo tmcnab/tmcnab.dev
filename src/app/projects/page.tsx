becef8d69518a9e25c7c92d80602da92bf97264e
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2024-10-05 23:49:34 -0400

    yagni - remove header

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,21 +1,10 @@
-import Flex from '@/components/Flex'
 import ListItem from './ListItem'
 import projects from '@/content/projects/projects'
 
 export default function Projects () {
 	return (
-		<>
-			<header>
-				<Flex>
-					<input placeholder='Search' />
-					<div>
-						<button>date</button>
-					</div>
-				</Flex>
-			</header>
-			<ul style={{ listStyle: 'none' }}>
-				{projects.map(ListItem)}
-			</ul>
-		</>
+		<ul style={{ listStyle: 'none' }}>
+			{projects.map(ListItem)}
+		</ul>
 	)
 }
